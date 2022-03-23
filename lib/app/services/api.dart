3214261e69f8688b7e5083c8b897dbef62708cbb
Karan <karansnarula@gmail.com>
2022-03-23 18:22:11 +0700

    Added more API stuffs

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,5 +1,13 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_keys.dart';
 
+enum Endpoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class API {
   final String apiKey;
   API({required this.apiKey});
@@ -9,4 +17,15 @@ class API {
   static const String host = 'ncov2019-admin.firebaseapp.com';
 
   Uri tokenUri() => Uri(scheme: 'https', host: host, path: 'token');
+
+  Uri endpointUri(Endpoint endpoint) =>
+      Uri(scheme: 'https', host: host, path: _paths[endpoint]);
+
+  static final Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'casesSuspected',
+    Endpoint.casesConfirmed: 'casesConfirmed',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+  };
 }
--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -20,4 +20,34 @@ class APIService {
     }
     throw response;
   }
+
+  Future<int> getEndpointData(
+      {required String accessToken, required Endpoint endpoint}) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http.get(
+      uri,
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(
+          response.body); // The content is a List of Map [{'test' : 123,},]
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint] as String;
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    throw response;
+  }
+
+  static final Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
 }
